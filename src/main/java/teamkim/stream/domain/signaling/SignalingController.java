edeae80b275d766b0e1c726b2ce01c99447f764d
anjdydhody <a01066444534@gmail.com>
2025-03-20 13:57:38 +0900

    feat: handleOffer 메소드 작성
    #1

---
+++ src/main/java/teamkim/stream/domain/signaling/SignalingController.java
@@ -0,0 +1,20 @@
+package teamkim.stream.domain.signaling;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class SignalingController {
+
+    @MessageMapping("/offer/{camNum}")
+    @SendTo("/topic/offer/{camNum}")
+    public String handleOffer(@Payload String offer, @DestinationVariable(value = "camNum") String camNum) {
+        log.info("[OFFER] cam{} : {}", camNum, offer);
+        return offer;
+    }
+}
